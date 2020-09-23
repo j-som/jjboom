4ce245e8a14c31b418b5ddebe6ad19008cda617f
j-som <308401057@qq.com>
2020-09-23 11:17:24 +0800

    Update c2s.erl

--- jgamesv-erl/apps/jgateway/src/net/c2s.erl
+++ jgamesv-erl/apps/jgateway/src/net/c2s.erl
@@ -76,6 +76,7 @@ code_change(_OldVsn, State, _Extra) ->
 read(Reader, BinData, Socket) ->
     case Reader:decode(BinData) of 
         {ok, Data, Rest1} ->
+            Reader:service(Data, Socket),
             read(Reader, Rest1, Socket);
         wait ->
             {ok, BinData}
